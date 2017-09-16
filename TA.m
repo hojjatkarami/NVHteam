f92d736934a874e8c442739555919ffa1c7c79ce
Hojat <Hojat@Hojat-PC>
2017-09-16 21:07:46 +0430

    TA is being added

--- TA.m
+++ TA.m
@@ -1,27 +1,52 @@
 %% Transmitted Acceleration Optimization %%
-function TA(stage)
-load('workspace.mat')
+function TA(g,g.stage0,g.stage1,Result_Parameters)
+
+cmd('********* TA *********');
+cmd('TA parameters are being set...');
+n=stage1.n;
+T = stage1.T;
+T1 = stage1.T1;
+F = stage1.F;
+
+stage1.init.x = stage0.opt.x;
+x_init = stage1.init.x;
+lb = (F==0).*(stage1.init.x - stage1.purt .* ( stage1.init.x - stage0.lb));
+ub = (F==0).*(stage1.init.x + stage1.purt .* ( stage0.ub - stage1.init.x));
+stage1.lb=lb;
+stage1.ub=ub;
+
+
+lb = lb(lb~=0);
+ub = ub(ub~=0);
+
+
 TA_Options.DeltaStatic = g.DeltaStatic;
 TA_Options.TAWeight = 0.9;
 TA_Options.KEDWeight = 0.1;
-TA_Options.PenFuncWeight = stage.PenFuncWeight;
-TA_Options.Lb = lb(1:27);
-TA_Options.Ub = ub(1:27);
-TA_Options.SwarmSize = stage.swarmsize;
-TA_Options.MaxFuncEval = stage.MaxFuncEval;
-TA_Options.FuncTol = stage.FuncTol;
-TA_Options.MaxIter = stage.MaxIter;
-TA_Options.FreqLowerBound = f_nat_lb;
-TA_Options.FreqUpperBound = f_nat_ub;
-TA_Options.Mass = M;
-TA_Options.SuspensionStruct = sus;
-TA_Options.Eta = eta;
-TA_Options.Omega = omega;
-TA_Options.Fhat = Fhat;
+TA_Options.PenFuncWeight = stage1.PenFuncWeight;
+% TA_Options.Lb = lb(1:27);
+% TA_Options.Ub = ub(1:27);
+TA_Options.SwarmSize = stage1.swarmsize;
+TA_Options.MaxFuncEval = stage1.MaxFuncEval;
+TA_Options.FuncTol = stage1.FuncTol;
+TA_Options.MaxIter = stage1.MaxIter;
+TA_Options.FreqLowerBound = g.lb_freq;
+TA_Options.FreqUpperBound = g.ub_freq;
+TA_Options.Mass = g.eng.M;
+TA_Options.SuspensionStruct = g.sus;
+% TA_Options.Eta = eta;
+TA_Options.Omega = g.eng.omega;
+TA_Options.Fhat = g.eng.Fhat;
 TA_Options.MountSelector=[1; 1; 1];    % dim(3*1):used to choose which directions are going to be accounted [mount1, mount2, mount3]
 TA_Options.OptTypeSelector = [1; 0; 0]; %This variable could be [1; 0; 0], [0; 1; 0] or [0; 0; 1] for 'max', 'sum' or 'norm', default value is 'max'!
 
-[x_opt_TA, Fval_TA] = TA_Optimizer(TA_Options);
-Res_TA = Result_Calc(x_opt_TA, Result_Parameters);
+[x_opt_TA, Fval_TA] = TA_Optimizer(TA_Options,n,T,F,x_init,T1,lb,ub);
+
+cmd('calculating results ...');
+
+stage1.init.result = stage0.opt.result;
+
+stage1.opt.x =  T * (F .* x_init + T1*x_opt_TA');
+stage1.opt.result = Result_Calc(stage1.opt.x,T, Result_Parameters);
 
 end
--- TA_Optimizer.m
+++ TA_Optimizer.m
@@ -1,10 +1,26 @@
-function [x_opt, Fval] = TA_Optimizer(TA_Opti)
-
-FitnessFcn3 = @(x) TA(x, TA_Opti.MountSelector, TA_Opti.OptTypeSelector, TA_Opti.Eta, TA_Opti.Omega, TA_Opti.Fhat, TA_Opti.Mass, TA_Opti.SuspensionStruct, TA_Opti.TAWeight, TA_Opti.KEDWeight, TA_Opti.PenFuncWeight);
+function [x_opt, Fval] = TA_Optimizer(TA_Opti,n,T,F,x_init,T1,lb,ub);
+cmd('TA_Optimizer started ...');
+%% PSO
+% options
+cmd('PSO options are being set...')
 TA_PSOoptions = optimoptions(@particleswarm,'PlotFcn',{@pswplotbestf},'SwarmSize',TA_Opti.SwarmSize,'FunctionTolerance',TA_Opti.FuncTol,'MaxIterations',TA_Opti.MaxIter);
-[x_opt3,Fval] = particleswarm(FitnessFcn3,27,TA_Opti.Lb,TA_Opti.Ub,TA_PSOoptions);
-x_opt3 = x_opt3';
-FitnessFcn33 = @(x) TA(x, TA_Opti.MountSelector, TA_Opti.OptTypeSelector, TA_Opti.Eta, TA_Opti.Omega, TA_Opti.Fhat, TA_Opti.Mass, TA_Opti.SuspensionStruct, 1, 0, 0);
+% start
+FitnessFcn3 = @(x) obj_TA(x,T,F,x_init,T1, TA_Opti.MountSelector, TA_Opti.OptTypeSelector, TA_Opti.Omega, TA_Opti.Fhat, TA_Opti.Mass, TA_Opti.SuspensionStruct, TA_Opti.TAWeight, TA_Opti.KEDWeight, TA_Opti.PenFuncWeight);
+cmd('TA PSO started...')
+[x_opt3,Fval] = particleswarm(FitnessFcn3,n,lb,ub,TA_PSOoptions);
+%% Hybrid fmincon
+
+% options
+cmd('fmincon options are being set...')
 fminconOptions = optimoptions(@fmincon,'Display','iter','MaxFunctionEvaluations',TA_Opti.MaxFuncEval);
+
+% start
+cmd('fmincon hybrid started');
+
+FitnessFcn33 = @(x) obj_TA(x, TA_Opti.MountSelector, TA_Opti.OptTypeSelector, TA_Opti.Eta, TA_Opti.Omega, TA_Opti.Fhat, TA_Opti.Mass, TA_Opti.SuspensionStruct, 1, 0, 0);
 x_opt = fmincon(FitnessFcn33,x_opt3,[],[],[],[],TA_Opti.Lb-1e-5,TA_Opti.Ub+1e-5,...
     @(x) nlcn(x, TA_Opti.Mass, TA_Opti.FreqLowerBound, TA_Opti.FreqUpperBound, TA_Opti.DeltaStatic),fminconOptions);
+
+
+
+end
