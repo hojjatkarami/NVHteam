145df36e3b67df2c485f929584d7484da7efa49f
Your Name <you@example.com>
2017-09-17 16:17:33 +0430

    'StiffLocBody' global variable added.

--- stiff_cal.m
+++ stiff_cal.m
@@ -1,5 +1,10 @@
 function [K,C,k_1,k_2,k_3,c_1,c_2,c_3] = stiff_cal(x)
 
+global StiffLocBody
+kLocBody_1 = StiffLocBody.k1;
+kLocBody_2 = StiffLocBody.k2;
+kLocBody_3 = StiffLocBody.k3;
+
 r_1 = x(1:3);
 r_2 = x(4:6);
 r_3 = x(7:9);
@@ -34,12 +39,54 @@ A_3 = [cos(o_3(3))*cos(o_3(2)) -sin(o_3(3))*cos(o_3(1))+cos(o_3(3))*sin(o_3(2))*
 
 % Stiffness
 k_1 = A_1*k_l_1*A_1';
+kDiag = diag(k_1);
+kDiag3D = diag(kDiag);
+k_1 = k_1 - kDiag3D;
+kDiag = (kDiag.*kLocBody_1)/(kDiag + kLocBody_1);
+kDiag3D = diag(kDiag);
+k_1 = k_1 + kDiag3D;
+
 k_2 = A_2*k_l_2*A_2';
+kDiag = diag(k_2);
+kDiag3D = diag(kDiag);
+k_2 = k_2 - kDiag3D;
+kDiag = (kDiag.*kLocBody_2)/(kDiag + kLocBody_2);
+kDiag3D = diag(kDiag);
+k_2 = k_2 + kDiag3D;
+
 k_3 = A_3*k_l_3*A_3';
+kDiag = diag(k_3);
+kDiag3D = diag(kDiag);
+k_3 = k_3 - kDiag3D;
+kDiag = (kDiag.*kLocBody_3)/(kDiag + kLocBody_3);
+kDiag3D = diag(kDiag);
+k_3 = k_3 + kDiag3D;
+
 K = [k_1 k_1*B_1' ; (k_1*B_1')' B_1*k_1*B_1'] + [k_2 k_2*B_2' ; (k_2*B_2')' B_2*k_2*B_2'] + [k_3 k_3*B_3' ; (k_3*B_3')' B_3*k_3*B_3'];
 
 % Damping
 c_1 = A_1*c_l_1*A_1';
+cDiag = diag(c_1);
+cDiag3D = diag(cDiag);
+c_1 = c_1 - cDiag3D;
+cDiag = (cDiag.*cLocBody_1)/(cDiag + cLocBody_1);
+cDiag3D = diag(cDiag);
+c_1 = c_1 + cDiag3D;
+
 c_2 = A_2*c_l_2*A_2';
+cDiag = diag(c_2);
+cDiag3D = diag(cDiag);
+c_2 = c_2 - cDiag3D;
+cDiag = (cDiag.*cLocBody_2)/(cDiag + cLocBody_2);
+cDiag3D = diag(cDiag);
+c_2 = c_2 + cDiag3D;
+
 c_3 = A_3*c_l_3*A_3';
+cDiag = diag(c_3);
+cDiag3D = diag(cDiag);
+c_3 = c_3 - cDiag3D;
+cDiag = (cDiag.*cLocBody_3)/(cDiag + cLocBody_3);
+cDiag3D = diag(cDiag);
+c_3 = c_3 + cDiag3D;
+
 C = [c_1 c_1*B_1' ; (c_1*B_1')' B_1*c_1*B_1'] + [c_2 c_2*B_2' ; (c_2*B_2')' B_2*c_2*B_2'] + [c_3 c_3*B_3' ; (c_3*B_3')' B_3*c_3*B_3'];
