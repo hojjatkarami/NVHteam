7dc7756bd6c4dbdfad522e62b6acd65c5117c512
Your Name <you@example.com>
2017-11-29 21:06:53 +0330

    nlcn code slightly has been changed.

--- nlcn.m
+++ nlcn.m
@@ -1,21 +1,24 @@
-function [c, ceq] = nlcn(x,T,F,x_init,T1, M, f_nat_lb, f_nat_ub, delta_s, StaticTests)
+function F = nlcn(x,T,F,x_init,T1, M_e, f_nat_lb, f_nat_ub, delta_s, StaticTests, st2dy, KEDcr)
+% 960908 Code Readed
 
 x_main = T * (F .* x_init + T1*x');
 [K,~] = stiff_cal(x_main,1);
 
-KEF = KEF_cal(K(1:6,1:6),M(1:6,1:6));
+KEF = KEF_cal(K(1:6,1:6),M_e(1:6,1:6));
 
-best_index = zeros(6,1);
-for j = 1:6 
-    [~,best_index(j)] = max(KEF(:,j));
-end
+KED = max(KEF)';
 
-KED = [KEF(best_index(1),1); KEF(best_index(2),2); KEF(best_index(3),3); KEF(best_index(4),4); KEF(best_index(5),5); KEF(best_index(6),6)];
+% best_index = zeros(6,1);
+% for j = 1:6 
+%     [~,best_index(j)] = max(KEF(:,j));
+% end
+
+% KED = [KEF(best_index(1),1); KEF(best_index(2),2); KEF(best_index(3),3); KEF(best_index(4),4); KEF(best_index(5),5); KEF(best_index(6),6)];
 
 % stc_Mat = zeros(6,8);
 
 
-mg = M(1,1)*9.81;
+mg = M_e(1,1)*9.81;
 
 % stc_Mat(:,1) = (abs((K(1:6,1:6)/1.5)^(-1))) * mg * [0;0;-1;0;0;0];
 % stc_Mat(:,2) = (abs((K(1:6,1:6)/1.5)^(-1))) * mg * [-0.4;0;-1;0;0;0];
@@ -29,16 +32,23 @@ mg = M(1,1)*9.81;
 mat = cell2mat(StaticTests(:,2:end));
 
 
-stc_Mat = (abs((K(1:6,1:6)/1.5)^(-1))) * mg * [mat';zeros(3,size(mat,1))];
+stc_Mat = (abs((K(1:6,1:6)/st2dy)^(-1))) * mg * [mat';zeros(3,size(mat,1))];
 
+% stc = max(stc_Mat')';
 stc = max(stc_Mat,[],2);
 
-f_nat = NF_Calculator(x_main,M(1:6,1:6));
+f_nat = NF_Calculator(x_main,M_e(1:6,1:6));
 
-A = [95;95;95;95;95;95]-KED;
+A = sum((KEDcr*ones(6,1)-KED).^2);
+
 B = stc - delta_s;
+P_s = heaviside(B)'*B;
+
 C = f_nat - f_nat_ub;
 D = f_nat_lb - f_nat;
+P_low = heaviside(D)'*D;
+P_High = heaviside(C)'*C;
+G =  P_low + P_High;
 
-c = [A; B; C; D];
-ceq = [];
+F = a*A + b*P_s + c*G;
+
